ce8aa4d75ce44c8bb6999dcf57f3a9e8b11d42c7
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-02-14 12:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+
+
+
+btn.addEventListener("click", function onClick(params) {
+	search.style.border = "2px solid black";
+});
+
